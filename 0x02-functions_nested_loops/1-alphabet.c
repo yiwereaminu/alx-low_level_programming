73bc2231067f670acb1847f3e9cffbd1ef4dd7cf
yiwereaminu <yiwereamin@gmail.com>
2023-10-10 16:01:59 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * this program print the alphabets all in lovwecase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char letter;
+		for (letter = 'a'; letter <= 'z'; letter++)
+		       _putchar(letter);
+		_putchar('\n');
+	}
+	return (0);	
+}
