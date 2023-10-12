e0e8d18c901338c521708c189167ab95658af8b4
yiwereaminu <yiwereamin@gmail.com>
2023-10-12 10:19:14 -0700

    leap

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,9 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && convert_day(month, day) >= 60)
+		if (month >= 2 && day >= 60)
 		{
-			day = convert_day(month, day);
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -24,16 +24,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && convert_day(month, day) == 60)
+		if (month == 2 && day == 60)
 		{
 			if (year % 4 != 0)
 			{
-				day = convert_day(month, day);
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 			}
 		else
 		{
-			day = convert_day(month, day);
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
