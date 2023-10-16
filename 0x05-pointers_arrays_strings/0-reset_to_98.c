e0909dbed7b1b4beeafa92aa86aeedc82d63ce0e
yiwereaminu <yiwereamin@gmail.com>
2023-10-16 12:00:53 -0700

    Reset int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Entry point
+ *@*n: pointer variable
+ *
+ * Return: Always 0.
+ */
+void reet_to_98(int *n)
+{
+	int i;
+	n = &i;
+	*n = 98;
+
+	_putchar(*n);
+}
