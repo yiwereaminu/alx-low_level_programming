a5f89cce342e11a1a78856dac97b57959c444cc8
yiwereaminu <yiwereamin@gmail.com>
2023-10-26 19:09:58 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,16 @@
 
 void _puts_recursion(char *s)
 {
-	if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+	if (*s =='\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else if (*s == ',' || *s == ' ' || *s == '.' || *s == '?')
+		_putchar(*s);
+	else
 	{
 		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-		_putchar('\n');
 	}
+	_puts_recursion(s + 1);
 }
