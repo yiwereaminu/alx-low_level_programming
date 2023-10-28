a8c30e1c486eaf13e9e7fad7a180c57baa74acb9
yiwereaminu <yiwereamin@gmail.com>
2023-10-27 17:41:18 -0700

    Prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - entry point
+* this program prints it name
+* Return - the name of the program
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
