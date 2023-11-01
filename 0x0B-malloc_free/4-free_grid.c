3589a8fd04d6f8130454c6d5b88c0b36a94c5d0d
yiwereaminu <yiwereamin@gmail.com>
2023-10-31 21:01:55 -0700

    frees the memory allocated for the the grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * free_grid - frees the memory allocate for the grid
+ * free_grid - frees the memory allocated for the grid
  * created by alloc_grid()
  * @grid: grid to free
  * @height: height of the grid
