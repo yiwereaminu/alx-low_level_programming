93a270cf5b4b098566f8df1326751df7a0c33efc
yiwereaminu <yiwereamin@gmail.com>
2023-11-06 21:35:04 -0800

    Add dog.h and readme.md files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a type that represent a dog
+* @name: char pointer for name of dog
+* @age: float var represent age of dog
+* @owner: cahr pointer for name of dog owner
+*
+* Description: this struct represents a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
